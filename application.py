f73487beb69af55a8e16743684dba8257d6b0ab1
C <earthhominid@gmail.com>
2021-04-15 17:37:45 -0300

    added html templates

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/") 
 def index():
-    return "hello,world"
+    return render_template('index.html', name=request.args.get("name", "world"))
+
